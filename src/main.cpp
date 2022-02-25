3d5cddfb90db925afec475a93d30801bbb1d5222
aa <aa>
2022-02-25 09:34:57 +0000

    initial debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <zconf.h>
 #include "ELSED.h"
 
 inline void
@@ -18,8 +19,17 @@ int main() {
   std::cout << "******************* ELSED main demo ******************" << std::endl;
   std::cout << "******************************************************" << std::endl;
 
+//  std::string img_path = "../images/P1020829.jpg";
+//  std::string img_path = "../images/building.jpg";
+//  std::string img_path = "../images/image-2021-12-23-00-45-38.png";
+//  std::string img_path = "../images/image-2022-01-02-22-33-49.png";
+//  std::string img_path = "../images/image-2021-12-22-22-21-13.png";
+  // range image, intensity image, 3 outdoor scenes
+  std::string img_path = "../images/range.png";
+//  std::string img_path = "/mnt/data/reconstruction/bundlefusion/copyroom/copyroom/frame-000000.color.jpg";
+
   // Using default parameters (long segments)
-  cv::Mat img = cv::imread("../images/P1020829.jpg");
+  cv::Mat img = cv::imread(img_path);
   if (img.empty()) {
     std::cerr << "Error reading input image" << std::endl;
     return -1;
@@ -29,12 +39,12 @@ int main() {
   upm::Segments segs = elsed.detect(img);
   std::cout << "ELSED detected: " << segs.size() << " (large) segments" << std::endl;
 
-  drawSegments(img, segs, CV_RGB(0, 255, 0), 2);
+  drawSegments(img, segs, CV_RGB(0, 255, 0), 1);
   cv::imshow("ELSED long", img);
-  cv::waitKey();
+//  cv::waitKey();
 
   // Not using jumps (short segments)
-  img = cv::imread("../images/P1020829.jpg");
+  img = cv::imread(img_path);
   if (img.empty()) {
     std::cerr << "Error reading input image" << std::endl;
     return -1;
@@ -46,9 +56,14 @@ int main() {
   segs = elsed_short.detect(img);
   std::cout << "ELSED detected: " << segs.size() << " (short) segments" << std::endl;
 
-  drawSegments(img, segs, CV_RGB(0, 255, 0), 2);
-  cv::imshow("ELSED short", img);
-  cv::waitKey();
-
+  drawSegments(img, segs, CV_RGB(0, 255, 0), 1);
+//  cv::imshow("ELSED short", img);
+//  cv::waitKey(100000);
+  while(1) {
+    std::cout << ".";
+    sleep(1);
+    cv::imshow("ELSED short", img);
+  cv::waitKey(100000);
+  }
   return 0;
 }
